c5b61c2f6b099f0209f1d340b50b2388f1116b38
Michael <micaheledquilan@gmail.com>
2024-08-11 21:07:32 -0400

    added notes

--- filmon-project/src/App.jsx
+++ filmon-project/src/App.jsx
@@ -36,10 +36,14 @@ function App() {
 Bugs*
 the filtered students don't get updated when one is deleted, updates on pg refresh or change in searchbar
 potential solution- make student display a child of app, and use usecontext to pass the data instead of props
+second potential solution - solve two bird 1 stone - have a check on delete, so you dont accidently delete students
+ie a popup to confirm that you want to delete this student
+also delete should only be visible to teacher
 Next steps
 1. add image implementation - elias 8/7 recording
 2. implement use context to pass info to different pages/routes -dylan
 3. better mobile ui scaling
+4. change routes
 extra
 1. admin/teacher
 2. login
